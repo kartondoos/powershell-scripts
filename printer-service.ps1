0a5a5bb6d6770be0f153e35c57c7da2742326ce3
kartondoos <51476593+kartondoos@users.noreply.github.com>
2021-07-04 00:27:12 +0200

    Add files via upload
    PrintNightmare-CVE-2021-1675
    script to see if print service is running

---
+++ printer-service.ps1
@@ -0,0 +1,25 @@
+﻿cls
+$printer = Get-Service -Name Spooler
+if ($printer.Status -eq 'Running')
+{
+    Write-Host "printer services is running" -ForegroundColor red
+    $stop = Read-Host stop printer spooler? y/n
+        if ($stop -eq 'y')
+        {
+        Stop-Service Spooler
+            $printer = Get-Service -Name Spooler
+            if ($printer.Status -ne 'Running')
+            {
+                Write-Host "print spooler is stoped" -ForegroundColor green
+            }
+        }
+        elseif ($stop -eq 'n')
+        {
+        write-host "aborted" -ForegroundColor red
+        Write-Host "printer service is still running" -ForegroundColor green
+        }
+}  
+elseif ($printer.Status -ne 'Running')
+{
+Write-Host "printer service is not running" -ForegroundColor green
+}
