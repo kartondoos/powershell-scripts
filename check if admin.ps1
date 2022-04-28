4e6a5db336af089d4e8d68783bf64895751f4fd2
kartondoos <51476593+kartondoos@users.noreply.github.com>
2022-04-28 11:59:26 +0200

    Update check if admin.ps1

--- check if admin.ps1
+++ check if admin.ps1
@@ -5,3 +5,6 @@ Write-Host "Dit is geen ADMIN"
 If (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
 Write-Host "Dit is een ADMIN"
 }
+
+#self elevating admin promt
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
